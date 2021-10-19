be032cb9431be55fa49191827d2f00d7b9978287
INDRA\fgrubio <fgrubio@minsait.com>
2021-10-19 16:46:03 +0200

    cambios para ver tag

--- src/app/tablaDynamic.component.ts
+++ src/app/tablaDynamic.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dynamic',
   template: `<app-tabla></app-tabla>`,
+  // template: `<app-tabla [reglas]='"reglasJuego"'></app-tabla>`,
   
 })
 export class TablaDynamicComponent  {
